4adb69898a4918c2dd23e3e1ab81de600bd9221f
Adriel Gama <adrielgama@gmail.com>
2019-01-25 09:39:37 -0300

    Update q4.c

--- q4.c
+++ q4.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define TAM 3
 
-void ler3Numeros(int vet[], int tam){
-	
-	int i;
-	
-	for(i = 0; i < tam; i++){
-		printf("Insira a entrada do usuario %d: ", i + 1);
-		scanf("%d", &vet[i]);
-	}
-	printf("\n");
+void ler3Numeros(int vet[]){
+    int i;
+    
+    for(i = 0; i < TAM; i++){
+        printf("Informe o %d valor: ", i + 1);
+        scanf("%d", &vet[i]);
+        printf("\n");
+    }
+
+    for(i = 0; i < TAM; i++){
+        printf("\nO %d numero inserido foi: %d", i + 1, vet[i]);
+    }
+    printf("\n\n");
+
 }
 
 
 int main(){
-	int vet[3], tam = 3;
-	
-	ler3Numeros(vet, tam);
-	
-	for(int i = 0; i < tam; i++){
-		printf("User %d = %d\n", i + 1, vet[i]);
-	}
-	
-	return 0;
+    int vet[TAM];
+
+    ler3Numeros(vet);
+
+
+    return 0;
 }
